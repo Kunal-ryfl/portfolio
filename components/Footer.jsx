ed70d75a538ca2bb8c3dcb6b96f1933813bf3aa3
Kunal <73430123+Kunal-ryfl@users.noreply.github.com>
2022-11-08 14:52:48 +0530

    Update Footer.jsx

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -13,8 +13,8 @@ const Footer = () => {
             // sx={{ marginTop: 2 }}
             href="https://www.linkedin.com/in/kunal-bhardwaj-b07b87217/"
           >
-            {" "}
-            <LinkedInIcon />{" "}
+            
+            <LinkedInIcon />
           </Button>
           <Button
             className="btn"
@@ -22,8 +22,8 @@ const Footer = () => {
             color="error"
             href="mailto:kunalsharma070806@gmail.com"
           >
-            {" "}
-            <MailIcon />{" "}
+            
+            <MailIcon />
           </Button>
           <Button
             className="btn"
@@ -31,8 +31,8 @@ const Footer = () => {
             color="secondary"
             href="https://github.com/Kunal-ryfl"
           >
-            {" "}
-            <GitHubIcon />{" "}
+ 
+            <GitHubIcon />
           </Button>
         </div>
          <p> Developed by Kunal Bhardwaj © 2022 </p>
